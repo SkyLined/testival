ad0838fb4be56fc203c53408c580252055308530
berendjanwever <berendjanwever@gmail.com>
2011-01-02 16:18:19 +0000

    Make test code more robust

--- writeconsole-shellcodes/test-w32-writeconsole-shellcode.bin.cmd
+++ writeconsole-shellcodes/test-w32-writeconsole-shellcode.bin.cmd
@@ -3,12 +3,12 @@
 ECHO     + Checking shellcode for NULL bytes:
 
 ECHO       + w32-writeconsole-shellcode.bin
-BETA3 h --nullfree w32-writeconsole-shellcode.bin > nul
+CALL BETA3.cmd h --nullfree w32-writeconsole-shellcode.bin > nul
 IF ERRORLEVEL 1 GOTO :FAILED
 
 ECHO     + Running shellcode:
 ECHO       + w32-writeconsole-shellcode.bin
-w32-testival [$]=ascii:w32-writeconsole-shellcode.bin eip=$ --EH 2>&1 | match_output "^Hello, world![\r\n]*$" --verbose > nul
+w32-testival [$]=ascii:w32-writeconsole-shellcode.bin eip=$ --EH 2>&1 | CALL match_output.cmd "^Hello, world![\r\n]*$" --verbose > nul
 IF ERRORLEVEL 1 GOTO :FAILED
 
 EXIT /B %ERRORLEVEL%
