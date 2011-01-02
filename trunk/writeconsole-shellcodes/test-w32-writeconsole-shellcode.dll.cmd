4b99cbe1fef60708777af3b1cc7747578a83ce0a
berendjanwever@gmail.com <berendjanwever@gmail.com@bea96d36-fded-11de-b9d1-2d015cf73129>
2011-01-02 16:18:19 +0000

    Make test code more robust

--- trunk/writeconsole-shellcodes/test-w32-writeconsole-shellcode.dll.cmd
+++ trunk/writeconsole-shellcodes/test-w32-writeconsole-shellcode.dll.cmd
@@ -1,8 +1,7 @@
 @ECHO OFF
 
 ECHO     + Testing w32-writeconsole-shellcode.dll:
-
-w32-testival.exe --loadlibrary w32-writeconsole-shellcode.dll 2>&1 | match_output "^Hello, world![\r\n]*$" --verbose >nul
+w32-testival.exe --loadlibrary w32-writeconsole-shellcode.dll 2>&1 | CALL match_output.cmd "^Hello, world![\r\n]*$" --verbose >nul
 IF ERRORLEVEL 1 GOTO :FAILED
 
 EXIT /B %ERRORLEVEL%
