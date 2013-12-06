749ebe7b283e80960c3b8ceda3060eeed093eab7
berendjanwever@gmail.com <berendjanwever@gmail.com@bea96d36-fded-11de-b9d1-2d015cf73129>
2013-12-06 16:06:08 +0000

    - Switch to new build folder structure - Switch to C++ - Require Windows XP are later (Windows 5.2) in order to do VEH on x64 - Add some missing malloc return type casts

--- trunk/defaults.h
+++ trunk/defaults.h
@@ -1,8 +1,8 @@
 #ifndef _defaults_h_
   #define _defaults_h_
-  // Target Windows 5.0 and up (Windows 2000). Required to be able to use VEH.
-  #define WINVER 0x0500
-  #define _WIN32_WINNT 0x0500
+  // Target Windows 5.2 and up (Windows XP). Required to be able to use VEH.
+  #define WINVER 0x0502
+  #define _WIN32_WINNT 0x0502
   // Include required header files:
   #include <windows.h>
   #include <string.h>
--- trunk/test-w64-testival.cmd
+++ trunk/test-w64-testival.cmd
@@ -2,7 +2,7 @@
 
 ECHO     + Testing w64-testival.exe:
 
-w64-testival.exe >nul
+"%~dp0build\exe\w64-testival.exe" >nul
 IF ERRORLEVEL 1 (
   ECHO       * Cannot test w64-testival.exe on x86 platform.
   EXIT /B 0
@@ -12,27 +12,27 @@ IF ERRORLEVEL 1 (
 )
 
 ECHO       + JMP to RIP
-w64-testival.exe a[$]="%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.bin" rip=$ --EH 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
+"%~dp0build\exe\w64-testival.exe" a[$]="%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.bin" rip=$ --EH 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
 IF ERRORLEVEL 1 GOTO :FAILED
 
 ECHO       + RET to RIP
-w64-testival.exe a[$]="%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.bin" rip=$ --ret --EH 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
+"%~dp0build\exe\w64-testival.exe" a[$]="%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.bin" rip=$ --ret --EH 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
 IF ERRORLEVEL 1 GOTO :FAILED
 
 ECHO       + Set RSP and ret-into-libc
-w64-testival.exe [$+2000]=$+2008 a[$+2008]="%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.bin" rsp=$+2000 --ret --EH --mem:size=4000 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
+"%~dp0build\exe\w64-testival.exe" [$+2000]=$+2008 a[$+2008]="%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.bin" rsp=$+2000 --ret --EH --mem:size=4000 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
 IF ERRORLEVEL 1 GOTO :FAILED
 
 ECHO       + Set RSP and JMP to RIP
-w64-testival.exe a[$+2008]="%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.bin" rsp=$+2000 rip=$+2008 --EH --mem:size=4000 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
+"%~dp0build\exe\w64-testival.exe" a[$+2008]="%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.bin" rsp=$+2000 rip=$+2008 --EH --mem:size=4000 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
 IF ERRORLEVEL 1 GOTO :FAILED
 
 ECHO       + Detect NULL pointer execution AV
-w64-testival.exe rip=0 --EH 2>&1 | CALL match_output.cmd "Second chance access violation while executing \[0x0000000000000000\]: no memory allocated\.[\r\n]*" --verbose >nul
+"%~dp0build\exe\w64-testival.exe" rip=0 --EH 2>&1 | CALL match_output.cmd "Second chance access violation while executing \[0x0000000000000000\]: no memory allocated\.[\r\n]*" --verbose >nul
 IF ERRORLEVEL 1 GOTO :FAILED
 
 ECHO       + Load DLL with writeconsole shellcode
-w64-testival.exe --loadlibrary "%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.dll" 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
+"%~dp0build\exe\w64-testival.exe" --loadlibrary "%CD%\writeconsole-shellcodes\w64-writeconsole-shellcode.dll" 2>&1 | CALL match_output.cmd "Hello, world![\r\n]*" --verbose >nul
 IF ERRORLEVEL 1 GOTO :FAILED
 
 EXIT /B %ERRORLEVEL%
